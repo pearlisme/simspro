a59f4842ebb327ec226453ca3c3a297b76a046c2
Pearl <muthubharathi.r@gmail.com>
2018-04-14 00:16:05 +0530

    Added index file.
    Signed-off-by: Pearl <muthubharathi.r@gmail.com>

--- src/main/java/com/spring/sims/simspro/SimsproApplication.java
+++ src/main/java/com/spring/sims/simspro/SimsproApplication.java
@@ -2,13 +2,12 @@ package com.spring.sims.simspro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.spring.sims.simspro.controller")
 public class SimsproApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SimsproApplication.class, args);
     }
 }
--- src/main/java/com/spring/sims/simspro/controller/HomeController.java
+++ src/main/java/com/spring/sims/simspro/controller/HomeController.java
@@ -4,18 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String goHome(){
-        return "Hello world!";
+    public String login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return "login";
+    }
+
+    @RequestMapping(value = "/index")
+    public String goHome() {
+
+        return "login";
     }
 
 
-    @RequestMapping("/help")
-    public String getHelp(){
+    @RequestMapping(value = "/help")
+    public String getHelp() {
+
         return "Helpline number +001 ";
     }
 }
