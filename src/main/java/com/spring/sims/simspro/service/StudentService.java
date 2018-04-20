67a9ecc53055a9ffbbffc8e1d7652dd89421f979
pearlM <Muthubharathi.r@gmail.com>
2018-04-21 01:04:21 +0530

    Added student service,model and repository
    Signed-off-by: pearlM <Muthubharathi.r@gmail.com>

--- src/main/java/com/spring/sims/simspro/service/StudentService.java
+++ src/main/java/com/spring/sims/simspro/service/StudentService.java
@@ -1,8 +1,38 @@
 package com.spring.sims.simspro.service;
 
+import com.spring.sims.simspro.model.Student;
+import com.spring.sims.simspro.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
 
 @Service
+@RequestMapping("/student")
 public class StudentService {
+    @Autowired
+    StudentRepository studentRepository;
+
+    @RequestMapping(value = "/create",method = RequestMethod.GET)
+    public void create(Student student){
+        studentRepository.save(student);
+    }
+
+    @RequestMapping(value = "/find/${id}",method = RequestMethod.GET)
+    public List<Student> find(@PathVariable Long id){
+        studentRepository.findAllById(id);
+    }
+
+    @RequestMapping(value = "/delete/${id}",method = RequestMethod.GET)
+    public void delete(@PathVariable Long id){
+        studentRepository.findAllById(id);
+    }
+
+
+
 
 }
