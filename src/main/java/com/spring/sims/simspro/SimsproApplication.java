81f666c5546b579068ccc019cd541211afec170e
pe@rl <Muthubharathi.r@gmail.com>
2018-06-30 13:10:51 +0530

    Added JSF support

--- src/main/java/com/spring/sims/simspro/SimsproApplication.java
+++ src/main/java/com/spring/sims/simspro/SimsproApplication.java
@@ -1,9 +1,18 @@
 package com.spring.sims.simspro;
 
+import com.sun.faces.config.ConfigureListener;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import scope.ViewScope;
+
+import javax.faces.webapp.FacesServlet;
+import java.util.HashMap;
 
 @SpringBootApplication
 //@EnableAutoConfiguration
@@ -13,5 +22,46 @@ public class SimsproApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SimsproApplication.class, args);
+    }
+
+
+    @Bean
+    public static ViewScope viewScope() {
+        return new ViewScope();
+    }
+
+    /**
+     * Allows the use of @Scope("view") on Spring @Component, @Service and @Controller
+     * beans
+     */
+    @Bean
+    public static CustomScopeConfigurer scopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+        hashMap.put("view", viewScope());
+        configurer.setScopes(hashMap);
+        return configurer;
+    }
+
+    /*
+     * Below sets up the Faces Servlet for Spring Boot
+     */
+    @Bean
+    public FacesServlet facesServlet() {
+        return new FacesServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                facesServlet(), "*.xhtml");
+        registration.setName("FacesServlet");
+        return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<ConfigureListener>(
+                new ConfigureListener());
     }
 }
